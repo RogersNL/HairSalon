95524753d420ccf826536499d9cb9d9dd25c54a5
“Nick <amellocello@msn.com>
2018-07-13 13:17:36 -0700

    Ninth controller test passes, tenth controller test fails as expected.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,8 +21,8 @@ namespace HairSalon.Controllers
     public ActionResult CreateForm()
     {
       List<Stylist> listStylists = Stylist.GetAll();
-      return new EmptyResult(); //Test 1 will fail
-      // return View(0); //Test 2 will fail
+      // return new EmptyResult(); //Test 1 will fail
+      return View(0); //Test 2 will fail
       // return View(listStylists); //Test will pass
     }
     // [HttpPost("/items")]
